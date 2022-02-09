730f475c023580dd9808c9945157d2fcf697d3b0
Guest400123064 <lgfz1003@icloud.com>
2022-02-09 13:37:00 -0500

    .

---
+++ pennguin/coref/word_level.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Author: Yuxuan Wang
+# Email: wangy49@seas.upenn.edu
+# Date: 02-09-2021
+# =============================================================================
+"""
+This module implements helper classes for co-reference resolution
+"""
+
+
+import transformers
+
+
