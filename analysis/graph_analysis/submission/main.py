4d141d490213086101db835413057f72fbd4a9fc
Guest400123064 <lgfz1003@icloud.com>
2022-03-09 12:51:43 -0500

    refactor

--- analysis/graph_analysis/submission/main.py
+++ analysis/graph_analysis/submission/main.py
@@ -11,21 +11,18 @@ import leidenalg as la
 from pyvis.network import Network as VisNetwork
 
 
-def draw_partition(
-    g: ig.Graph, 
-    partition: List[FrozenSet[int]]
-) -> VisNetwork:
+def draw_partition(g: ig.Graph, partition: List[FrozenSet[int]]) -> VisNetwork:
+    """Helper function to generate HTML visualizations"""
 
     net = VisNetwork('1024px', '1024px')
-    for i_grp, grp in enumerate(partition):
-        for i_node in grp:
-            net.add_node(
-                i_node, 
-                value=2,
-                label=g.vs[i_node]['name'],
-                group=i_grp,
-                title=f'Group {i_grp}'
-            )
+    for i_node, i_grp in enumerate(partition):
+        net.add_node(
+            i_node, 
+            value=2,
+            label=g.vs[i_node]['name'],
+            group=i_grp,
+            title=f'Group {i_grp}'
+        )
             
     for edge in g.es:
         net.add_edge(
@@ -35,19 +32,34 @@ def draw_partition(
             color='red' if (edge['edge_weight'] < 0) else 'blue'
         )
 
-    net.repulsion(central_gravity=0.1, spring_length=256)
-    net.show_buttons(filter_=True)
+    net.repulsion(central_gravity=0.1, spring_length=512)
+    net.show_buttons(filter_=['nodes', 'edges', 'physics'])
     return net
 
 
 # ======================================================================================================
-# Load raw co-mention edge list and merge accross articles
-#   IMPORTANT: Though there are fields <co_mentions_sum> 
-#       and <co_mentions_count> in the source edge list file, 
-#       they are PER ARTICLE statistics. So we still need 
-#       to sum them up individually and manually calculate average
-df_edge = df_edge = (
-    pd.read_csv('rw_tone_merge.csv')
+# Load raw co-mention edge list and get a set of people (for plotting)
+df_edge = pd.read_csv('rw_tone_merge.csv')
+ppl_set = set(
+    df_edge.loc[np.logical_and(df_edge.loc[:, 'flag_person'] == 1, df_edge.loc[:, 'flag_company'] == 0), 'id1']
+        .unique()
+        .tolist()
+).union({
+    # Special cases
+    'robin bairstow',
+    'celestin rwabukumba',
+    'celestin twahirwa',
+    'tongai maramba',
+    'umurenge saccos'
+})
+
+# Merge accross articles
+# IMPORTANT: Though there are fields <co_mentions_sum> 
+#   and <co_mentions_count> in the source edge list file, 
+#   they are PER ARTICLE statistics. So we still need 
+#   to sum them up individually and manually calculate average
+df_edge = (
+    df_edge
         .dropna(subset='id2')
         .groupby(['id1', 'id2', 'flag_person', 'flag_company'], as_index=False, sort=False)
             [['co_mentions_sum', 'co_mentions_count']]
@@ -78,6 +90,7 @@ class PeopleNetwork:
         id2: str         = 'id2',
         edge_weight: str = 'weight'
     ) -> None:
+
         node_list = pd.concat([df[id1], df[id2]]).unique() 
         edge_list = zip(df[id1], df[id2])
         edge_attr = {'edge_weight': df[edge_weight].values.tolist()}
@@ -86,16 +99,24 @@ class PeopleNetwork:
         self.g.add_vertices(node_list)
         self.g.add_edges(es=edge_list, attributes=edge_attr)
         
-        self.ppl_set = set(node_list.tolist())
+        # TODO: In the future we should NOT use string names!
+        #   Because the names are NOT gauranteed to be UNIQUE;
+        #     instead, we need to pre-fix a unique id for 
+        #     each entity and use that as igraph node names.
+        self.existing_names = set(node_list.tolist())
         
         
     def itos(self, idx: int) -> str:
+        """Index to String. Given a node id, return the 
+            node name, e.g., 42 -> Tony"""
         
         node = self.g.vs[idx]
         return node['name']
     
     
     def stoi(self, name: str) -> int:
+        """String to Index. Given a node name, find the FIRST id 
+            corresponds to that name, e.g., Tony -> 42"""
         
         found = self.g.vs.select(name=name)
         if len(found) == 1:
@@ -108,9 +129,7 @@ class PeopleNetwork:
     def signed_partition(self, 
         resolution_pos: float = 0.01,
         resolution_neg: float = 0.3,
-        return_member:  bool  = False,
-        return_name:    bool  = False,
-    ) -> Union[List[int], List[FrozenSet[int]], List[FrozenSet[str]]]:
+    ) -> List[int]:
         
         g_pos = self.g.subgraph_edges(self.g.es.select(edge_weight_gt=0), delete_vertices=False)
         g_neg = self.g.subgraph_edges(self.g.es.select(edge_weight_lt=0), delete_vertices=False)
@@ -128,30 +147,26 @@ class PeopleNetwork:
         )
         
         optimizer = la.Optimiser()
+        optimizer.set_rng_seed(42)
         optimizer.optimise_partition_multiplex(
             [part_pos, part_neg],
             layer_weights=[1, -1]
         )
         
-        if return_member:
-            return part_pos.membership
-        if return_name:
-            return [frozenset(map(self.itos, c)) for c in part_pos]
-        return [frozenset(c) for c in part_pos]
-    
+        return part_pos.membership
+
 
     def signed_partition_merge_orgs(self, 
         node_list: List[str], 
         edge_list: List[Tuple[str, str, float]],
         ppl_membership: List[int],
-        return_name: bool     = False,
         resolution_pos: float = 0.01,
         resolution_neg: float = 0.3
-    ) -> Tuple[ig.Graph, Union[List[FrozenSet[int]], List[FrozenSet[str]]]]:
+    ) -> Tuple[ig.Graph, List[int]]:
         
         # Merge org nodes & edges
         g_merge = copy.deepcopy(self.g)
-        g_merge.add_vertices(list(set(node_list) - self.ppl_set))
+        g_merge.add_vertices(list(set(node_list) - self.existing_names))
         for s, t, w in edge_list:
             g_merge.add_edge(s, t, edge_weight=w)
             
@@ -177,30 +192,43 @@ class PeopleNetwork:
         )
         
         optimizer = la.Optimiser()
+        optimizer.set_rng_seed(42)
         optimizer.optimise_partition_multiplex(
             [part_pos, part_neg],
             layer_weights=[1, -1],
             is_membership_fixed=fix_membership
         )
         
-        if return_name:
-            return g_merge, [frozenset(map(self.itos, c)) for c in part_pos]
-        return g_merge, [frozenset(c) for c in part_pos]
+        return g_merge, part_pos.membership
 
 # %%
+# Clustering over people-co-mentions first
 net_ppl = PeopleNetwork(df_edge_ppl, edge_weight='score_average_weighted')
-mem_ppl = net_ppl.signed_partition(resolution_neg=0.02, return_member=True)
+mem_ppl = net_ppl.signed_partition(resolution_neg=0.02)
 
+# Extend existing people-only graph with co-mentions involving companies
+#   <g_merge> is the extended graph
+#   <mem_org> is a list with same length as number of nodes; each element is the cluster id 
+#       to which the corresponding node is assigned
 node_list = pd.concat([df_edge_org['id1'], df_edge_org['id2']]).unique() 
 edge_list = zip(df_edge_org['id1'], df_edge_org['id2'], df_edge_org['score_average_weighted'])
+g_merge, mem_org = net_ppl.signed_partition_merge_orgs(node_list, edge_list, mem_ppl)
 
-g_merge, partition = net_ppl.signed_partition_merge_orgs(node_list, edge_list, mem_ppl)
-net = draw_partition(g_merge, partition)
-
-for n in net_ppl.ppl_set:
-    n = net_ppl.stoi(n)
-    net.node_map[n]['shape'] = 'square'
+# Generate html vis
+net_vis = draw_partition(g_merge, mem_org)
+for n in ppl_set:
     
-net.show('tmp.html')
+    # Code peoples to squares and leave companies as dots
+    found = g_merge.vs.select(name=n)
+    if len(found) == 1:
+        n = found[0].index
+        net_vis.node_map[n]['shape'] = 'square'
+    
+net_vis.show('rw_tone_cls.html')
 
 # %%
+# Write cluster assignments
+df_assign = pd.DataFrame({
+    'entity_name': ,
+    'cluster': mem_org
+})
